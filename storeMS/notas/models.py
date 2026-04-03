bcf358a233a7c1f61e8cab716dd825df07ad77a6
Camilo Martins <camilo.s.martins.oliva@gmail.com>
2026-04-02 23:00:38 -0300

    Creacoión modelos: Proveedores, Productos y Notas

--- storeMS/notas/models.py
+++ storeMS/notas/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Nota(models.Model):
+    
+    admin_id = models.IntegerField(db_index=True)  # dueño / jefe (auth-ms)
+    nombre_nota = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    observaciones = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre_nota
+    
+    class Meta:
+        db_table='notas'
+        verbose_name='Notas'
+        verbose_name_plural='Notas'
